be3e258526957c0262699164ab91c0ebd1bddb20
root <root@hercules.(none)>
2009-08-22 13:29:56 -0300

    Seguimos con la clase. Traladamos muchos parametros dentro de options

--- trunk/examples/example_01/js/example-01.js
+++ trunk/examples/example_01/js/example-01.js
@@ -2,11 +2,10 @@ window.addEvent ('domready', function (){
   $imagenes = $('moodoo-sls').getChildren();
   $imagenes.each (function ($picture, $iP) {
     $picture.setStyles ({
+      left: $iP*50,
       'background-position': $iP*(-390).toString()+ 'px top'
     });
   });
 
-  window.addEvent ('load', function (){
     new mooDooSLS ('moodoo-sls');
-  });
 });
--- trunk/examples/example_01/js/mooDooSLS.class.js
+++ trunk/examples/example_01/js/mooDooSLS.class.js
@@ -1,23 +1,27 @@
- 
 var mooDooSLS = new Class({
   Implements: [Events, Options],
-    
+
   options: {
-    transitionTime: 1000,
-    faseTransitionTime: 0,
+    frameTime: 1000,
+    transitionTime: 500,
+    transitionDelay: 200,
+
     autoplay:true,
     delayAutoplay: 1000,
+
     repeat:true,
     random: false,
     indexFirstImg: 0,
-    framesNode: 'moodoo-sls'
+    framesParentNode: 'moodoo-sls'
   },
-    
+
   // Constructor
-  initialize: function(id_nodo, options){
+  initialize: function($node_id, options){
     // Seteamos elementos de la clase.
     this.setOptions(options);
+
     // Configuracion Inicial de Clase
+    this.nodeContainer = $node_id ? $($node_id) : $(this.options.framesParentNode);
     this.configuracionInicial();
 
     if (this.options.autoplay) {
@@ -28,46 +32,49 @@ var mooDooSLS = new Class({
   },
 
   configuracionInicial: function () {
-    this.nodeContainer = $(this.options.framesNode);
+    // Referencia de nodos
+    this.childrenNodes=this.nodeContainer.getChildren();
 
-    this.images=this.nodeContainer.getChildren();
-    this.imageN = this.images[0];
-    this.imageN1 = this.images[1];
-    this.images.setStyles({
+    this.nodeN0 = this.childrenNodes[0];
+    this.nodeN1 = this.childrenNodes[1];
+
+    this.childrenNodes.setStyles({
       opacity: 0
     });
-		
-    this.images[0].setStyles({
+
+    this.childrenNodes[0].setStyles({
       opacity: 1
     });
 
-    this.fxN = new Fx.Morph(this.imageN, {
+    this.fxN0 = new Fx.Morph(this.nodeN0, {
       duration: this.options.transitionTime,
       onComplete:function(el){
-        this.fxN.index= (this.fxN.index+1 > this.images.length-1) ? (this.fxN.index+1-this.images.length) : (this.fxN.index+1);
-        this.fxN.element=this.images[this.fxN.index];
-        this.fxN.subject=this.images[this.fxN.index];
+        this.fxN0.index= (this.fxN0.index+1 > this.childrenNodes.length-1) ? (this.fxN0.index+1-this.childrenNodes.length) : (this.fxN0.index+1);
+        this.fxN0.element=this.childrenNodes[this.fxN0.index];
+        this.fxN0.subject=this.childrenNodes[this.fxN0.index];
 
         (function(){
-          this.fxN.start({
+          this.fxN0.start({
             opacity: [1, 0]
           });
-          this.fxN.element.setStyles({
+          this.fxN0.element.setStyles({
             'z-index': 0
           })
-        }).delay (this.options.transitionTime, this);
+        }).delay (this.options.frameTime, this);
 
       }.bind(this)
     });
-    this.fxN.index=0;
-		
-    this.fxN1 = new Fx.Morph(this.imageN1,{
+    this.fxN0.index=0;
+
+
+    this.fxN1 = new Fx.Morph(this.nodeN1,{
       duration:this.options.transitionTime,
       onComplete:function(el){
-        this.fxN1.index = (this.fxN1.index+1 > this.images.length-1) ? (this.fxN1.index+1-this.images.length) : (this.fxN1.index+1);
-        this.fxN1.element=this.images[this.fxN1.index];
-        this.fxN1.subject=this.images[this.fxN1.index];
+        this.fxN1.index = (this.fxN1.index+1 > this.childrenNodes.length-1) ? (this.fxN1.index+1-this.childrenNodes.length) : (this.fxN1.index+1);
+        this.fxN1.element=this.childrenNodes[this.fxN1.index];
+        this.fxN1.subject=this.childrenNodes[this.fxN1.index];
 
+        // Disparamos el segundo efecto con un delay
         (function(){
           this.fxN1.start({
             opacity: [0, 1]
@@ -75,19 +82,21 @@ var mooDooSLS = new Class({
           this.fxN1.element.setStyles({
             'z-index': 1000
           })
-        }).delay (this.options.transitionTime + this.options.faseTransitionTime, this);
+        }).delay (this.options.frameTime, this);
 
 				
       }.bind(this)
     });
     this.fxN1.index=1;
   },
-	
-  play:function(){
 
-    this.fxN.start({
-      opacity: [1, 0]
-    });
+  play:function() {
+
+    (function(){
+      this.fxN0.start({
+        opacity: [1, 0]
+      });
+    }).delay (this.options.transitionDelay, this);
 
     this.fxN1.start({
       opacity: [0, 1]
@@ -95,13 +104,14 @@ var mooDooSLS = new Class({
   },
 
   stop:function(){
-		
+
   },
   endFxAnimation:function(){
     this.play();
   },
+
   pause: function () {
-    this.fxN.pause();
+    this.fxN0.pause();
     this.fxN1.pause();
   }
 });
